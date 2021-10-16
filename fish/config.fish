1d6406bca6bf2b2336681908b56d3f49eb6abfa2
Matthieu Bessat <spamfree@matthieubessat.fr>
2021-10-16 16:41:36 +0200

    fix: sync fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,16 @@
+set PATH ~/.npm-global/bin $PATH
+set PATH ~/.gem/ruby/2.7.0/bin $PATH
+set PATH ~/go/bin $PATH
+set PATH ~/.local/bin $PATH
+
+export DENO_INSTALL="/home/mbess/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
+
 # Clipboard management
-# install xclip before
-# with `sudo pacman -S xclip`
 alias c='xclip -selection c'
 alias p='xclip -selection c -o'
+
+alias list='ls -A -l --color=never'
+alias fde='firefox-developer-edition'
+
+export GTK_THEME="Adwaita:dark"
